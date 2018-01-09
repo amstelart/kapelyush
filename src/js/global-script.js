6f791963e470065ea39ede2546fbadf0ae5f3c3e
Артем <amstelart@MacBook-Pro-Artem.local>
2018-01-09 15:33:27 +0300

    шота

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,5 +1,5 @@
 // Если на проекте jQuery
-$( document ).ready(function() {
+$(document).ready(function() {
   // code
   $("#main-slider").owlCarousel({
     items: 1,
@@ -16,30 +16,57 @@ $( document ).ready(function() {
     loop: true,
     center: false,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-    responsive : {
-      0 : {
+    responsive: {
+      0: {
         items: 1,
         nav: true,
         loop: true,
         center: true,
       },
-      480 : {
+      480: {
         items: 3,
       },
-      768 : {
+      768: {
         items: 3,
       },
-      992 : {
+      992: {
         items: 3,
       },
-      1200 : {
+      1200: {
         items: 4,
       },
-      1800 : {
+      1800: {
         items: 4,
       }
     }
   });
+
+  // noUiSlider
+  var handlesSlider = document.getElementById('sort-slider');
+
+  noUiSlider.create(handlesSlider, {
+    tooltips: false,
+    start: [700, 5590],
+    connect: true,
+    range: {
+      'min': [700],
+      'max': [5590]
+    }
+  });
+
+  var nodes = [
+  	document.getElementById('lower-value'), // 0
+  	document.getElementById('upper-value')  // 1
+  ];
+
+  // Display the slider value and how far the handle moved
+  // from the left edge of the slider.
+  handlesSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
+  	nodes[handle].innerHTML = values[handle];
+  });
+
+
+
 });
 
 // Изоляция без jQuery
