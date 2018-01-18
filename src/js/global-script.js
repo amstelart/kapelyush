eee47ca8389279ed81124dedae9c0c84e477db7c
Артем <amstelart@MacBook-Pro-Artem.local>
2018-01-19 00:05:54 +0300

    add all

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -41,27 +41,97 @@ $(document).ready(function() {
     }
   });
 
-  // noUiSlider
-  // var handlesSlider = document.getElementById('sort-slider');
-  //
-  // noUiSlider.create(handlesSlider, {
-  //   tooltips: false,
-  //   start: [700, 5590],
-  //   connect: true,
-  //   range: {
-  //     'min': [700],
-  //     'max': [5590]
+  // $(".thumbnails-additional").owlCarousel({
+  //   items: 3,
+  //   nav: true,
+  //   dots: false,
+  //   margin: 10,
+  //   loop: true,
+  //   center: false,
+  //   navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+  //   responsive: {
+  //     0: {
+  //       items: 3,
+  //       nav: true,
+  //       loop: true,
+  //       center: false,
+  //     },
+  //     480: {
+  //       items: 3,
+  //     },
+  //     768: {
+  //       items: 3,
+  //     },
+  //     992: {
+  //       items: 3,
+  //     },
+  //     1200: {
+  //       items: 4,
+  //     },
+  //     1800: {
+  //       items: 4,
+  //     }
   //   }
   // });
-  //
-  // var nodes = [
-  // 	document.getElementById('lower-value'),
-  // 	document.getElementById('upper-value')
-  // ];
-  //
-  // handlesSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
-  // 	nodes[handle].innerHTML = values[handle];
-  // });
+
+
+  var thumbnails_owl = $('#product-product .thumbnails-additional');
+  thumbnails_owl.on('initialize.owl.carousel initialized.owl.carousel ' +
+    'initialize.owl.carousel initialize.owl.carousel ',
+    function(e) {
+      $("#product-product #gallery_01 .thumbnail").show();
+    });
+  thumbnails_owl.owlCarousel({
+    items: 4,
+    loop: true,
+    nav: true,
+    dots: false,
+    navSpeed: 1000,
+    margin: 20,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    responsive: {
+      0: {
+        items: 3,
+      },
+      480: {
+        items: 4,
+      },
+    },
+  });
+  $("#product-product .thumbnails img").elevateZoom({
+    gallery: "gallery_01",
+    galleryActiveClass: "active",
+    borderColour: "#000",
+  });
+  var thumbnails_additional = $('#product-product .thumbnails-additional .thumbnail');
+  thumbnails_additional.each(function() {
+    $(this).click(function() {
+      thumbnails_additional.removeClass('current-additional');
+      $(this).addClass('current-additional');
+    });
+  });
+
+  // noUiSlider
+  var handlesSlider = document.getElementById('sort-slider');
+
+  noUiSlider.create(handlesSlider, {
+    tooltips: false,
+    start: [700, 5590],
+    connect: true,
+    range: {
+      'min': [700],
+      'max': [5590]
+    }
+  });
+
+  var nodes = [
+    document.getElementById('lower-value'),
+    document.getElementById('upper-value')
+  ];
+
+  handlesSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
+    nodes[handle].innerHTML = values[handle];
+  });
 
   // Product slider
   $('#vertical').lightSlider({
@@ -78,6 +148,20 @@ $(document).ready(function() {
 
 
 });
+
+// Выезжающий поиск
+function searchToggle(obj, evt){
+  var container = $(obj).closest('.search-wrapper');
+  if(!container.hasClass('active')){
+    container.addClass('active');
+    evt.preventDefault();
+  }
+  else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+    container.removeClass('active');
+    // clear input
+    container.find('.search-input').val('');
+  }
+}
 
 // Изоляция без jQuery
 // (function(){
